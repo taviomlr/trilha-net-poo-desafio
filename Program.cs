0d296240feb0111b3c16eff5e8a1dcacfa1b9019
Távio Rodrigues <tavio.rodrigues@iob.com.br>
2024-04-29 09:56:33 -0300

    Criação dos atributos e parâmetros nas classes e criado os casos de testes.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("\n");
+System.Console.WriteLine("*************************************************************************");
+System.Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "(81) 9.9999-9999", modelo: "C3", imei: "121554548878", memoria: 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagran");
+System.Console.WriteLine("*************************************************************************");
+System.Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "(81) 9.8888-8888", modelo: "10", imei: "2254555666", memoria: 512);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Linkedin");
+System.Console.WriteLine("*************************************************************************");
+System.Console.WriteLine("\n");
+
+
